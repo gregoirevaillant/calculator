8390502b4da540082e66a79d2b6d56209b4bdaa6
Groune <98833627+gregoirevaillant@users.noreply.github.com>
2023-02-26 23:32:11 +0100

    Add functions

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-console.log('Set up!');
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
